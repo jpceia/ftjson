9d87e9b285966cdc3b2d572c049f3d56bb48c301
jpceia <joao.p.ceia@gmail.com>
2021-11-23 04:10:13 +0000

    started adding doxygen-like comments

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief   Frees an array of strings.
+ * 
+ * @param   array the array to free.
+ * @param   size  the size of the array.
+ */
 void string_array_free(char **array, int size)
 {
     for (int i = 0; i < size; ++i)
@@ -15,6 +21,14 @@ void json_move_whitespace(char **str)
         ++*str;
 }
 
+/**
+ * @brief   Realloc with error handling.
+ *          If the realloc fails, it clears the previous memory allocation.
+ * 
+ * @param   ptr     the pointer to the memory to reallocate.
+ * @param   size    the new size of the memory.
+ * @return  void*   the new pointer to the memory.
+ */
 void *realloc_free_on_fail(void *ptr, size_t size)
 {
     void *new_ptr = realloc(ptr, size);
@@ -24,7 +38,12 @@ void *realloc_free_on_fail(void *ptr, size_t size)
     return new_ptr;
 }
 
-// File to string conversion
+/**
+ * @brief   Gets the file content as a string.
+ * 
+ * @param   filename    the name of the file to convert.
+ * @return  char*       the string containing the file content.
+ */
 char *file_to_string(const char *filename)
 {
     FILE *fp = fopen (filename, "rb");
